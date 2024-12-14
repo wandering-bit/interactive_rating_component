19e26113245afe521bbc385bb7baf2912ed21e42
Ranjan Sharma <sranjansharma2001@gmail.com>
2024-12-14 18:51:48 +0530

    first submit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+
+let selection = 0
+
 const firstclick = (element) => {
     console.log(element.id)
 
@@ -9,5 +12,15 @@ const firstclick = (element) => {
     });
 
     element.classList.add('active')
+    selection = element.id
 }
 
+const btnsubmit = () => {
+    console.log("submitting");
+    console.log("selection",selection)
+    const element = document.querySelector('.result')
+    const spanElement = document.querySelector('.scoretext')
+    element.style.marginRight = '340px'
+    console.log(element)
+    spanElement.textContent = `You selected ${selection} out of 5` 
+}
